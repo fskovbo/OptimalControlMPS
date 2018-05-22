4f82eaf8955d4962b0f8174187263c9b30c6d105
fskovbo <f.skovbo@hotmail.com>
2018-05-22 17:02:02 +0200

    Added analysis for a series of bonddim

--- main/AnalyzeBondDim.cpp
+++ main/AnalyzeBondDim.cpp
@@ -14,15 +14,33 @@ using namespace itensor;
 
 int main(int argc, char* argv[])
 {
+    std::vector<double> maxBondDim = {10 , 20 , 25 , 30 , 35 , 40 , 100 };
+
+    if (argc < 3)
+    {
+        printfln("Usage: %s InputFile_BHcontrol BHrampInitialFinal.txt",argv[0]);
+        printfln("No input detected ... using standard parameters");
+    }
+
+    double tstep  = 1e-2;
+    double T      = 6;
+
+    int N         = 8;
+    int Npart     = 8;
+    int locDim    = 8;
+
     // load InputFile_BHcontrol.txt
-    auto input    = InputGroup(argv[1],"input");
+    if (argc >= 2)
+    {
+        auto input  = InputGroup(argv[1],"input");
+        tstep       = input.getReal("tstep",1e-2);
+        T           = input.getReal("T",6);
 
-    double tstep  = input.getReal("tstep",1e-2);
-    double T      = input.getReal("T",6);
+        N           = input.getInt("N",8);
+        Npart       = input.getInt("Npart",8);
+        locDim      = input.getInt("d",8);    
+    }
 
-    int N         = input.getInt("N",8);
-    int Npart     = input.getInt("Npart",8);
-    int locDim    = input.getInt("d",8);
 
     double J      = 1.0;
     double U_i    = 2.0;
@@ -30,24 +48,28 @@ int main(int argc, char* argv[])
 
 
     // load BHrampInitialFinal.txt
-    std::string RampDataFile(argv[2]);
     std::vector<double> control, times;
-    double tmp, t, u;
-    std::ifstream in(RampDataFile);
-    while(!in.eof())
+    if (argc >= 3)
     {
-        in >> t >> tmp >> tmp >> u >> tmp;
-        times.push_back(t);
-        control.push_back(u);
+        std::string RampDataFile(argv[2]);
+        double tmp, t, u;
+        std::ifstream in(RampDataFile);
+        while(!in.eof())
+        {
+            in >> t >> tmp >> tmp >> u >> tmp;
+            times.push_back(t);
+            control.push_back(u);
+        }
+    } 
+    else
+    {
+        times   = SeedGenerator::generateRange(0,tstep,T);
+        control = SeedGenerator::adiabaticSeed(U_i,U_f,times.size());
     }
 
-    auto sites    = BoseHubbard(N,locDim);
-    auto psi_i    = InitializeState(sites,Npart,J,control.front());
-    auto psi_f    = InitializeState(sites,Npart,J,control.back());
-
-    auto stepper    = BH_tDMRG(sites,J,tstep,{"Cutoff=",1E-8});
-    OptimalControl<BH_tDMRG> OC(psi_f,psi_i,stepper,0);
-
+    auto sites      = BoseHubbard(N,locDim);
+    auto psi_i      = InitializeState(sites,Npart,J,control.front());
+    auto psi_f      = InitializeState(sites,Npart,J,control.back());
 
     // extend duration and controls
     for (size_t i = 1; i <= 50; i++) {
@@ -55,50 +77,64 @@ int main(int argc, char* argv[])
     control.push_back( control.back() );
     }
 
-    
-    // run time evolution and get fidelity
-    auto fid        = OC.getFidelityForAllT(control);
-    auto psi        = OC.getPsit();
-    
-
-    // Save extended Data
-
-    // Create an output string stream
-    std::ostringstream streamObj;
-
-    // Set Fixed -Point Notation
-    streamObj << std::fixed;
-
-    // Set precision to 1 digits
-    streamObj << std::setprecision(1);
-
-    //Add double to stream
-    streamObj << T;
-
-    std::string filename = "TimeEvolBondDimT" + streamObj.str() + ".txt";
-    std::ofstream myfile (filename);
-    size_t ind = 0;
-
-    if (myfile.is_open())
+    for (double maxM : maxBondDim)
     {
-        for (auto& pt : psi)
+        std::cout << "Calculating time-evolution for maxM = " << maxM << "\n";
+
+        auto stepper    = BH_tDMRG(sites,J,tstep,{"Cutoff=",1E-8,"Maxm=",maxM});
+        OptimalControl<BH_tDMRG> OC(psi_f,psi_i,stepper,0);
+        
+        // run time evolution and get fidelity
+        auto fid        = OC.getFidelityForAllT(control);
+        auto psi        = OC.getPsit();
+        
+
+        // Save extended Data
+
+        // Create an output string stream
+        std::ostringstream streamObj1;
+        // Set Fixed -Point Notation
+        streamObj1 << std::fixed;
+        // Set precision to 1 digits
+        streamObj1 << std::setprecision(1);
+        //Add double to stream
+        streamObj1 << T;
+        // Create an output string stream
+        std::ostringstream streamObj2;
+        // Set Fixed -Point Notation
+        streamObj2 << std::fixed;
+        // Set precision to 1 digits
+        streamObj2 << std::setprecision(1);
+        //Add double to stream
+        streamObj2 << maxM;
+
+        std::string filename = "TimeEvolBondDimT" + streamObj1.str() + "maxM" + streamObj2.str() + ".txt";
+        std::ofstream myfile (filename);
+        size_t ind = 0;
+
+        if (myfile.is_open())
         {
-            myfile << times.at(ind)     << "\t";
-            myfile << control.at(ind)   << "\t";
-            myfile << fid.at(ind)       << "\t";
-            
-            for(int b = 1; b < pt.N(); ++b) 
+            for (auto& pt : psi)
             {
-                myfile << linkInd(pt,b).m() << "\t"; 
+                double cfrac = correlationTerm(sites,pt,"Adag","A")/Npart;
+
+                myfile << times.at(ind)     << "\t";
+                myfile << control.at(ind)   << "\t";
+                myfile << fid.at(ind)       << "\t";
+                myfile << cfrac             << "\t";
+                
+                for(int b = 1; b < pt.N(); ++b) 
+                {
+                    myfile << linkInd(pt,b).m() << "\t"; 
+                }
+
+                ind++;
+                myfile << "\n";
             }
-
-            ind++;
-            myfile << "\n";
+            myfile.close();
         }
-        myfile.close();
-    }
-    else std::cout << "Unable to open file\n";
-
+        else std::cout << "Unable to open file\n";
+    } // foreach maxBondDim
 
     
     return 0;
